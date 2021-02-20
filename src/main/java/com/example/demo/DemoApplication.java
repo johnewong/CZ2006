bbb36be1ba5c663780737cb30e039fc5e469f391
yfweng <johnewong95@gmail.com>
2021-02-20 14:40:21 +0800

    first init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("驱动初始化成功");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
