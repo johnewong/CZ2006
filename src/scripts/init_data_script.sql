acc8aa6eef9d31698d3add1aa792c9a1803c239f
yfweng <johnewong95@gmail.com>
2021-02-27 21:23:26 +0800

    change create table script

--- src/scripts/init_data_script.sql
+++ src/scripts/init_data_script.sql
@@ -1,3 +1,2 @@
 insert into users values('1', 'user01','123456','2020-01-01','123@email.com','820000'
-,'male','G140022P','1','user02','2020-01-01',null,null,0)
-
+                        ,1,'G140022P','1',2,'2020-01-01',null,null,0)
