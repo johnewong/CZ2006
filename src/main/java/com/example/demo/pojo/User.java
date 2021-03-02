bce7c0e1671ceea855602ffcea631dc0bce36ecd
yfweng <johnewong95@gmail.com>
2021-03-02 16:37:20 +0800

    add user properties

--- src/main/java/com/example/demo/pojo/User.java
+++ src/main/java/com/example/demo/pojo/User.java
@@ -8,6 +8,8 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "users")
 @JsonIgnoreProperties({ "handler","hibernateLazyInitializer" })
@@ -16,15 +18,134 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "userid")
-    private int userID;
-
+    private Integer userID;
     @Column(name = "username")
     private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "birthdate")
+    private Date birthDate;
+    @Column(name = "emailaddress")
+    private String emailAddress;
+    @Column(name = "contactnumber")
+    private String contactNumber;
+    @Column(name = "gender")
+    private boolean gender;
+    @Column(name = "icnumber")
+    private String icNumber;
+    @Column(name = "usertype")
+    private Integer userType;
+    @Column(name = "createdby")
+    private Integer createdBy;
+    @Column(name = "createddate")
+    private Date createdDate;
+    @Column(name = "updatedby")
+    private Integer updatedBy;
+    @Column(name = "updateddate")
+    private Date updatedDate;
+    @Column(name = "isdeleted")
+    private boolean isDeleted;
 
-    public int getUserID() {
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getIcNumber() {
+        return icNumber;
+    }
+
+    public void setIcNumber(String icNumber) {
+        this.icNumber = icNumber;
+    }
+
+    public Integer getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Integer userType) {
+        this.userType = userType;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Integer getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Integer updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Integer getUserID() {
         return userID;
     }
-    public void setUserID(int userID) {
+    public void setUserID(Integer userID) {
         this.userID = userID;
     }
 
